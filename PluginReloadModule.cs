93773e73009f91a62105075f4d2b6ca93c236250
grzegrzk <grzegorz.kuligowski@gmail.com>
2020-01-05 20:23:15 +0100

    tests how to fix lack of plugin reloading in ksp 1.8.1

--- PluginReloadModule.cs
+++ PluginReloadModule.cs
@@ -15,7 +15,7 @@ using KramaxPluginReload.UI;
 using UnityEngine;
 using System.Reflection.Emit;
 using System.Threading;
-
+using System.Security.Cryptography;
 
 [KSPAddon(KSPAddon.Startup.Instantly, true)]
 public class KramaxPluginReloadModule : MonoBehaviour
@@ -97,13 +97,44 @@ namespace KramaxPluginReload
             }
         }
 
-        private static Assembly LoadAssembly(string location)
+        private static Assembly LoadAssembly(string location, int versionCount)
         {
             try
             {
-                byte[] assemblyBytes = System.IO.File.ReadAllBytes(location);
-                Assembly a = Assembly.Load(assemblyBytes);
-                Deb.Log("KramaxPluginReload: Reloaded assembly: {0} version: {1}.", a.GetName().Name, a.GetName().Version);
+                //Therere is a bug in Mono that prevents loading assembly with the same name more than once
+                //(if such assembly is loaded then old version is returned). The bug report can be 
+                //found here: https://xamarin.github.io/bugzilla-archives/11/11199/bug.html.
+                //The bug is corrected in mono-6.6.0.161 and mono-5.16.0.179 but mono used in KSP 1.8.1 is
+                //reported as "5.11.0 (Visual Studio built mono)". To hack around this we do the following:
+                // - Decompile .dll using ildasm
+                // - Change assembly name inside decomiled file (simple regex, fragment ".assembly <assembly-name>")
+                // - Compile file again to .dll using ilasm
+                // - Load changed .dll
+                //example location of ildasm is C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\
+                //example location of ilasm is C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+
+                // String tmpLocation = location + "-tmp-" + versionCount + ".dll";
+                //System.IO.File.Copy(location, tmpLocation);
+                //Assembly a = Assembly.Load(tmpLocation);
+                byte[] assemblyBytes = System.IO.File.ReadAllBytes(location);                
+                Assembly a = Assembly.Load(assemblyBytes);                
+                Type type = Type.GetType("Mono.Runtime");
+                if (type != null)
+                {
+                    MethodInfo dispalayName = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
+                    if (dispalayName != null)
+                    {
+                        Deb.Log("Mono version: {0}", dispalayName.Invoke(null, null));
+                    }
+                }
+                Deb.Log(
+                    "KramaxPluginReload: Reloaded assembly: {0} version: {1}, location: {2}, md5: {3}.", 
+                    a.GetName().Name, 
+                    a.GetName().Version,                   
+                    location, 
+                    GetMd5Hash(assemblyBytes)
+                    );
+                a.GetName().Name = a.GetName().Name + "-" + versionCount;
                 return a;
             }
             catch (Exception ex)
@@ -113,7 +144,20 @@ namespace KramaxPluginReload
             return null;
         }
 
-        static Type CreateUniqueSubClass(ModuleBuilder moduleBldr, Type originalType, int versionUid)
+        static string GetMd5Hash(byte[] input)
+        {
+            byte[] data = MD5.Create().ComputeHash(input);
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        static Type CreateUniqueSubClass(ModuleBuilder moduleBldr, Type originalType, int versionUid, KSPAddon kspAddon)
         {
 
             String newClassName = String.Format("{0}_{1}_", originalType.Name, versionUid);
@@ -124,9 +168,65 @@ namespace KramaxPluginReload
             TypeBuilder typeBldr =
                 moduleBldr.DefineType(newClassName, TypeAttributes.Public | TypeAttributes.Class, originalType);
 
-            ConstructorBuilder ctor = typeBldr.DefineDefaultConstructor(MethodAttributes.Public);
+            if (kspAddon != null) { 
+                CustomAttributeBuilder customAttributeBuilder =
+                    new CustomAttributeBuilder(
+                        typeof(KSPAddon).GetConstructor(new Type[] { typeof(KSPAddon.Startup), typeof(bool) }),
+                        new object[] { kspAddon.startup, kspAddon.once }
+                       );
+                typeBldr.SetCustomAttribute(customAttributeBuilder);
+            }
 
+            ConstructorBuilder ctor = typeBldr.DefineDefaultConstructor(MethodAttributes.Public);            
+            CreateMethodCallingSuper(typeBldr, originalType, "Start", MethodAttributes.Public | MethodAttributes.Virtual, typeof(void), new Type[0]);
+            CreateMethodCallingSuper(typeBldr, originalType, "OnDestroy", MethodAttributes.Public | MethodAttributes.Virtual, typeof(void), new Type[0]);
             return typeBldr.CreateType();
+        }
+
+
+        static void CreateMethodCallingSuper(
+            TypeBuilder typeBldr,
+            Type originalType,
+            String methodName,
+            MethodAttributes attributes,
+            Type returntype,
+            Type[] typeArguments)
+        {
+
+            MethodInfo baseMethod = originalType.GetMethod(methodName, typeArguments);
+            if (baseMethod == null)
+            {
+                Deb.Log("Method {0} does not exist in {1}, will not be supported", methodName, originalType);
+                return;
+            }
+            else
+            {
+                Deb.Log("Method {0} exists in {1}", methodName, originalType);
+            }
+            MethodBuilder startBuilder = typeBldr.DefineMethod(methodName, attributes, returntype, typeArguments);
+            ILGenerator ilGenerator = startBuilder.GetILGenerator();
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            if (typeArguments.Length >= 1)
+            {
+                ilGenerator.Emit(OpCodes.Ldarg_1);
+            }
+            if (typeArguments.Length >= 2)
+            {
+                ilGenerator.Emit(OpCodes.Ldarg_2);
+            }
+            if (typeArguments.Length >= 3)
+            {
+                ilGenerator.Emit(OpCodes.Ldarg_3);
+            }
+            if (typeArguments.Length >= 4)
+            {
+                for (int i = 4; i <= typeArguments.Length; ++i)
+                {
+                    ilGenerator.Emit(OpCodes.Ldarg_S, i);
+                }
+            }
+            ilGenerator.EmitCall(OpCodes.Call, baseMethod, new Type[0]);
+            ilGenerator.Emit(OpCodes.Ret);
         }
 
         private static void LoadPlugins()
@@ -160,88 +260,91 @@ namespace KramaxPluginReload
                     PluginClasses.Remove(r);
                 }
 
-                var assembly = LoadAssembly(setting.Path);
+                var assembly = LoadAssembly(setting.Path, versionCount);
 
-                //Remove assembly if reloading failed
-                if (assembly == null)
-                {
-                    foreach (PluginClass pluginClass in PluginClasses.Where(pc => pc.pluginSetting == setting).ToList())
-                    {
-                        PluginClasses.Remove(pluginClass);
-                    }
-                    continue;
-                }
+                ////Remove assembly if reloading failed
+                //if (assembly == null)
+                //{
+                //    foreach (PluginClass pluginClass in PluginClasses.Where(pc => pc.pluginSetting == setting).ToList())
+                //    {
+                //        PluginClasses.Remove(pluginClass);
+                //    }
+                //    continue;
+                //}
 
-                String tmpAssemblyName = String.Format("KramaxPIRLAsmb_{0}", versionCount);
-                String tmpModuleName = String.Format("KramaxPIRLMod_{0}", versionCount);
+                //String tmpAssemblyName = String.Format("KramaxPIRLAsmb_{0}", versionCount);
+                //String tmpModuleName = String.Format("KramaxPIRLMod_{0}", versionCount);
 
-                AssemblyBuilder assemblyBldr =
-                    Thread.GetDomain().DefineDynamicAssembly(new AssemblyName(tmpAssemblyName),
-                                                             AssemblyBuilderAccess.Run);
 
-                ModuleBuilder moduleBldr = assemblyBldr.DefineDynamicModule(tmpModuleName);
+                //Deb.Log("KramaxPluginReload: plugin: {0}, tmpAssemblyName: {1}, tmpModuleName: {2}.", setting.Name, tmpAssemblyName, tmpModuleName);
 
-                IList<Type> derivedClassess =
-                  (from t in assembly.GetTypes()
-                   where t.IsSubclassOf(typeof(MonoBehaviour))
-                   select t).ToList();
+                //AssemblyBuilder assemblyBldr =
+                //    Thread.GetDomain().DefineDynamicAssembly(new AssemblyName(tmpAssemblyName),
+                //                                             AssemblyBuilderAccess.Run);
+                
+                //ModuleBuilder moduleBldr = assemblyBldr.DefineDynamicModule(tmpModuleName);
 
-                List<PluginClass> plugins = new List<PluginClass>();
-                Dictionary<Type, Type> typeMapping = new Dictionary<Type, Type>();
+                //IList<Type> derivedClassess =
+                //  (from t in assembly.GetTypes()
+                //   where t.IsSubclassOf(typeof(MonoBehaviour))
+                //   select t).ToList();
 
-                foreach (var derivedClass in derivedClassess)
-                {
-                    Deb.Log("KramaxPluginReload.LoadPlugins: got type {0}.", derivedClass.Name);
-                    Deb.Log("KramaxPluginReload.LoadPlugins: from assembly {0}, v{1}.",
-                        derivedClass.Assembly.GetName().Name,
-                        derivedClass.Assembly.GetName().Version);
+                //List<PluginClass> plugins = new List<PluginClass>();
+                //Dictionary<Type, Type> typeMapping = new Dictionary<Type, Type>();
 
-                    System.Attribute[] attrs = System.Attribute.GetCustomAttributes(derivedClass);
+                //foreach (var derivedClass in derivedClassess)
+                //{
+                //    Deb.Log("KramaxPluginReload.LoadPlugins: got type {0}.", derivedClass.Name);
+                //    Deb.Log("KramaxPluginReload.LoadPlugins: from assembly {0}, v{1}.",
+                //        derivedClass.Assembly.GetName().Name,
+                //        derivedClass.Assembly.GetName().Version);
 
-                    KSPAddon kspAddon = null;
+                //    System.Attribute[] attrs = System.Attribute.GetCustomAttributes(derivedClass);
 
-                    foreach (var att in attrs)
-                    {
-                        if (att is KSPAddon)
-                        {
-                            kspAddon = att as KSPAddon;
-                        }
-                    }
+                //    KSPAddon kspAddon = null;
 
-                    if (!derivedClass.IsSubclassOf(typeof(KramaxReloadExtensions.ReloadableMonoBehaviour)))
-                    {
-                        Deb.Log("KramaxPluginReload.LoadPlugins: ERROR type {0} is not ReloadableMonoBehaviour subclass.",
-                            derivedClass.Name);
-                        continue;
-                    }
+                //    foreach (var att in attrs)
+                //    {
+                //        if (att is KSPAddon)
+                //        {
+                //            kspAddon = att as KSPAddon;
+                //        }
+                //    }
 
-                    if (kspAddon != null)
-                    {
-                        Deb.Log("KramaxPluginReload.LoadPlugins: type {0} will be top-level component.", derivedClass.Name);
+                //    if (!derivedClass.IsSubclassOf(typeof(KramaxReloadExtensions.ReloadableMonoBehaviour)))
+                //    {
+                //        Deb.Log("KramaxPluginReload.LoadPlugins: ERROR type {0} is not ReloadableMonoBehaviour subclass.",
+                //            derivedClass.Name);
+                //        continue;
+                //    }
 
-                        PluginClass pluginClass = new PluginClass();
+                //    if (kspAddon != null)
+                //    {
+                //        Deb.Log("KramaxPluginReload.LoadPlugins: type {0} will be top-level component.", derivedClass.Name);
 
-                        pluginClass.pluginSetting = setting;
-                        pluginClass.originalType = derivedClass;
-                        pluginClass.kspAddon = kspAddon;
-                        pluginClass.type = CreateUniqueSubClass(moduleBldr, derivedClass, versionCount);
+                //        PluginClass pluginClass = new PluginClass();
 
-                        plugins.Add(pluginClass);
-                    }
-                    else
-                    {
-                        Deb.Log("KramaxPluginReload.LoadPlugins: type {0} will be sub-level component.", derivedClass.Name);
+                //        pluginClass.pluginSetting = setting;
+                //        pluginClass.originalType = derivedClass;
+                //        pluginClass.kspAddon = kspAddon;
+                //        pluginClass.type = CreateUniqueSubClass(moduleBldr, derivedClass, versionCount, kspAddon);
 
-                        var newType = CreateUniqueSubClass(moduleBldr, derivedClass, versionCount);
-                        typeMapping[derivedClass] = newType;
-                    }
-                }
+                //        plugins.Add(pluginClass);
+                //    }
+                //    else
+                //    {
+                //        Deb.Log("KramaxPluginReload.LoadPlugins: type {0} will be sub-level component.", derivedClass.Name);
 
-                foreach (var plugin in plugins)
-                {
-                    plugin.typeMapping = typeMapping;
-                    PluginClasses.Add(plugin);
-                }
+                //        var newType = CreateUniqueSubClass(moduleBldr, derivedClass, versionCount, kspAddon);
+                //        typeMapping[derivedClass] = newType;
+                //    }
+                //}
+
+                //foreach (var plugin in plugins)
+                //{
+                //    plugin.typeMapping = typeMapping;
+                //    PluginClasses.Add(plugin);
+                //}
             }
 
             foreach (var pluginClass in PluginClasses)
